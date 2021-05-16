dfca3cf3195f47174fc83dbf1f581a21f44155f7
hidouche ait youcef <hidouche.aityoucef@gmail.com>
2021-05-16 16:44:56 +0200

    Modifications of the home page, contact page and projects page

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// animer te titre de la page ad'accueil
+let title = document.getElementByClassName("title");
+document.addEventListener("mouseover", function(){
+title.style.color = "red";
+  });
+alert("ddd");
+alert("fffr");
