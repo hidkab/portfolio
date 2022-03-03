c9161bf4c30fd1b1e4f28d481c73f04d333ac459
hidouche ait youcef <hidouche.aityoucef@gmail.com>
2022-03-03 09:50:39 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-// animer te titre de la page ad'accueil
-let title = document.getElementByClassName("title");
-document.addEventListener("mouseover", function(){
-title.style.color = "red";
-  });
-alert("ddd");
-alert("fffr");
+
+
+
+// add color to button on hover
+document.getElementById("btn-projects").onmouseover = function() 
+{
+  this.style.backgroundColor = "#999aaf";
+  this.style.color = "white";
+  this.style.borderRadius = "3rem";
+  this.style.boxShadow = "10px 15px 20px #999aaf";
+  
+}
+
